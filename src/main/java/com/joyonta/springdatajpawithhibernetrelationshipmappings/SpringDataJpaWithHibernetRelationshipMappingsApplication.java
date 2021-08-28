e18bfc2ed473944731859b8861ca37a064344626
Joyonta Saha <joyontasaha8@gmail.com>
2021-08-28 19:49:10 +0600

    initial commit

---
+++ src/main/java/com/joyonta/springdatajpawithhibernetrelationshipmappings/SpringDataJpaWithHibernetRelationshipMappingsApplication.java
@@ -0,0 +1,13 @@
+package com.joyonta.springdatajpawithhibernetrelationshipmappings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaWithHibernetRelationshipMappingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaWithHibernetRelationshipMappingsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joyonta/springdatajpawithhibernetrelationshipmappings/SpringDataJpaWithHibernetRelationshipMappingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joyonta.springdatajpawithhibernetrelationshipmappings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaWithHibernetRelationshipMappingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
